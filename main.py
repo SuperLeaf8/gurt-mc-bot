1b0d166c6686886cccdde0a16fee2ef2bbcc1fa9
Julian-Redacted <julianmarnewick21@gmail.com>
2023-05-19 02:55:46 +0200

    Update main.py
    added minecraft bullshit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 Token = os.getenv("Token")
 from discord.ext import commands
+import requests
 
 load_dotenv()
 
@@ -17,5 +18,22 @@ async def on_ready():
 async def hello(ctx):
     username = ctx.message.author.mention
     await ctx.send("Hello there " + username)
+
+@bot.command()
+async def minecraft(ctx):
+    r = requests.get('https://api.mcsrvstat.us/2/gurt.cc')
+    json_data = r.json()
+    online = str(json_data["online"])
+    playerCount = str(json_data["players"]["online"])
+    version = str(json_data["version"])
+    hostname = json_data["hostname"]
+
+    embed = discord.Embed(
+        title="Server Info",
+        description='Online: ' + online + '\nonline: ' + playerCount + '\nHostname: ' + hostname + '\nVersion: ' + version,
+        color=discord.Colour.dark_gray()
+    )
+
+    await ctx.send(embed=embed)
 
 bot.run(Token)
